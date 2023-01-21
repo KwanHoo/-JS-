a6026dd9572d8f1d854a5616db4ede5a3797a8aa
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2023-01-21 17:31:22 +0900

    #2.2 Variables

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert("test");
+// alert("test");
+
+console.log(3.141592);
+console.log("lalalalal");
+
+console.log(5+2);
+console.log(5*2);
+console.log(5/2);
+
+// variable 값을 저장하거나 유지하는 역할을 함
+const x = 24;
+console.log(x + 100);
+console.log(x * 100);
+console.log(x / 100);
+
+const myName = "mori";
+
+console.log("hello " + myName);
