e7191a8cfd79e98c26ea0c958b325766800dc300
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2023-01-21 19:59:17 +0900

    #2.5_Arrays

--- script.js
+++ script.js
@@ -28,3 +28,33 @@ let somethingNum; // undefiend
 console.log(empNo);
 
 console.log(somethingNum);
+
+//* array
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const daysOfWeek = mon + thu + wed + thu + fri + sat + sun;
+
+console.log(daysOfWeek);
+
+const daysOfWeeks = [mon , thu , wed , thu , fri , sat , sun]; //array
+
+const nonsense = [1,2, 'hello', false, null, true, undefined, "mori"];
+console.log(daysOfWeeks);
+console.log(nonsense);
+
+const daysOfWeekText = ['mon' , 'thu' , 'wed' , 'thu' , 'fri' , 'sat' , 'sun']; //array
+console.log(daysOfWeekText);
+console.log(daysOfWeekText[0]);
+
+// add one more day to the array
+daysOfWeekText.push('testDay');
+console.log(daysOfWeekText);
+
+const toBuy  = ["chair", "desk"];
+toBuy.push("monitor");
