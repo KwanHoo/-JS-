64fc7dc24c0aaa0fd5bb009541071a6591c37d73
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2023-01-23 16:31:03 +0900

    #3.2 Serching For Elements

--- script2.js
+++ script2.js
@@ -4,3 +4,19 @@ title.innerText ="Got you";
 
 console.dir(title.id);
 console.dir(title.className);
+
+const hellos = document.getElementsByClassName("Hello");
+
+console.log(hellos);
+
+
+const title3 = document.getElementsByTagName("h3");
+console.log(title3)
+
+const title3_1 = document.querySelector(".hello11 h4");
+console.log(title3_1);
+
+// 아래 두개는 같은 결과를 얻음
+//const title4_1 = document.querySelector("#test");
+//const title4_2 = document.getElementById("test");
+
