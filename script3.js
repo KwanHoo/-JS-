ab915e862aaa50f88fd676b9056b8743b236a7f4
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2023-01-24 20:47:21 +0900

    #4.1 Form Submission

--- script3.js
+++ script3.js
@@ -8,8 +8,21 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function handleBtnClick(){
-    console.dir(loginInput.value);
-    console.log("click!!!");
+    // console.dir(loginInput.value);
+    // console.log("click!!!");
+
+    const userName = loginInput.value;
+    console.log(userName);
+
+    //* JS를 이용할시 validation 체크
+    // if(userName === ""){
+    //     console.log("Please write your name!!");
+    //     alert("Please write your name!!!")
+    // }else if(userName.length > 15){
+    //     alert("Your name is too long.")
+    // }else{
+    //     console.log("good!!")
+    // }
 }
 
 loginButton.addEventListener("click", handleBtnClick)
