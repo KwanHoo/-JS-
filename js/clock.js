086f4910006fe5e401eac58350003afdf67a19f2
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2023-01-26 00:04:50 +0900

    #5.1 Timeouts and Dates

--- js/clock.js
+++ js/clock.js
@@ -6,4 +6,15 @@ function sayHello(){
     console.log("hello");
 }
 
-setInterval(sayHello, 5000); //5s
+//setInterval(sayHello, 5000); //5s
+
+setTimeout(sayHello, 5000);
+
+function getClock(){
+    const date = new Date();
+    // console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
+    clock.innerText = (`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
+}
+
+getClock();
+setInterval(getClock, 1000);
