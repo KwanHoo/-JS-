5c36054f5c1e3dfefd36e6aac26e89707b68913b
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2023-01-28 12:19:27 +0900

    #6.1 Background

---
+++ js/background.js
@@ -0,0 +1,19 @@
+const images = [
+    "0.jpg",
+    "1.jpg",
+    "2.jpg",
+    "3.jpg"
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+// console.log(chosenImage);
+
+const bgImage = document.createElement("img");
+
+// console.log(bgImage) //<img>
+
+bgImage.src = `img/${chosenImage}`;
+//console.log(bgImage); // <img src="img/2.jpg>"
+
+document.body.appendChild(bgImage);
