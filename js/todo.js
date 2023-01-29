145e26a54187d50c502a7ffb4c31ce259123b493
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2023-01-29 23:30:01 +0900

    #7.1 Adding todo

--- js/todo.js
+++ js/todo.js
@@ -3,12 +3,19 @@ const todoInput = toDoForm.querySelector("input");
 // const todoInput = document.querySelector("#todo-form input"); 둘이 동일
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo){
+    // console.log("I will paint ", newTodo);
+    const li = document.createElement("li");
+             
+}
+
 function handleToDOSubmit(event) {
     event.preventDefault();
-    console.log(todoInput.value);
+    // console.log(todoInput.value);
     const newTodo = todoInput.value;
     todoInput.value = "";
-    console.log(newTodo, todoInput.value);
+    // console.log(newTodo, todoInput.value);
+    paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDOSubmit);
