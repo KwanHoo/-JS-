e0f4423d6ef45da8de98fb34c9e17aa88894b7f8
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2023-02-05 14:28:12 +0900

    #8.1 Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,19 @@
+const API_KEY = '2834387742b25d5393a21e88fee8246a';
+
 function onGeoOk(position) {
     console.log(position);
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
     console.log("You live in", lat, lng)
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    // console.log(url);
+    fetch(url).then(response => response.json()).then(data => {
+        // console.log(data.name, data.weather[0].main)
+        const weather= document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 function onGeoError() {
     alert("Can't find you. No weather for you.")
